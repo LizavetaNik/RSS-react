7c4e7279c1bddee35956c156e68ddad7b2b8ef33
Lizaveta <lizabeth.nik@gmail.com>
2023-11-14 14:59:54 +0200

    fix: delete file

--- app/store/store.ts
+++ app/store/store.ts
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { charactersSlice } from "../features/charactersSlice";
+
 import { searchSlice } from "../features/valueSearchSlice";
 import { loadingSlice } from "../features/loadingSlice";
-import { charSlice } from "../features/charSlice";
+
 import { charactersApi } from '../features/charactersApi';
 import { charApi } from "../features/charApi";
 
@@ -10,11 +10,9 @@ export type RootState = ReturnType<typeof store.getState>;
 
 export const store = configureStore({
   reducer: {
-    characterss: charactersSlice.reducer,
     valueSearch: searchSlice.reducer,
     valueLoading: loadingSlice.reducer,
     valueLoadingItem: loadingSlice.reducer,
-    charItem: charSlice.reducer,
     [charactersApi.reducerPath]: charactersApi.reducer,
     [charApi.reducerPath]: charApi.reducer,
   },
